0a65b0a30b4c2f3253dd06c1711e38ac9e9abb01
cntt2-tamguyen <nhantam181@gmail.com>
2019-03-18 12:14:28 +0700

    up page

--- src/main/java/vn/edu/leading/shop/controllers/EmployeeController.java
+++ src/main/java/vn/edu/leading/shop/controllers/EmployeeController.java
@@ -64,12 +64,41 @@ public class EmployeeController {
         return "employees/list";
     }
 
-    @GetMapping("employees/search")
-    public String search(@RequestParam("term") String term, Model model) {
+    @GetMapping("/employees/search/{pageNumber}")
+    public String search(@RequestParam("term") String term, Model model, HttpServletRequest request,
+                         @PathVariable int pageNumber) {
         if (StringUtils.isEmpty(term)) {
             return "redirect:/employees";
         }
-        model.addAttribute("employees", employeeService.search(term));
+        List<EmployeeModel> list = employeeService.search(term);
+        if (list == null) {
+            return "redirect:/employee";
+        }
+        PagedListHolder<EmployeeModel> pages;
+        int pagesize = 3;
+
+        pages = new PagedListHolder<EmployeeModel>(list);
+        pages.setPageSize(pagesize);
+
+        final int goToPage = pageNumber - 1;
+        if (goToPage <= pages.getPageCount() && goToPage >= 0) {
+            pages.setPage(goToPage);
+        }
+
+        request.getSession().setAttribute("employeelist", pages);
+        int current = pages.getPage() + 1;
+        int begin = Math.max(1, current - list.size());
+        int end = Math.min(begin + 5, pages.getPageCount());
+        int totalPageCount = pages.getPageCount();
+        String baseUrl = "/employees/page/";
+
+        model.addAttribute("beginIndex", begin);
+        model.addAttribute("endIndex", end);
+        model.addAttribute("currentIndex", current);
+        model.addAttribute("totalPageCount", totalPageCount);
+        model.addAttribute("baseUrl", baseUrl);
+        model.addAttribute("employees", pages);
+
         return "employees/list";
     }
 
