f9bc76b341144ac29a1e5ca3334f3e9fce9db694
cntt2-tamguyen <nhantam181@gmail.com>
2019-04-17 12:19:40 +0700

    add example user

--- src/main/java/vn/edu/leading/shop/controllers/UserController.java
+++ src/main/java/vn/edu/leading/shop/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import vn.edu.leading.shop.models.UserModel;
 import vn.edu.leading.shop.repositories.RoleRepository;
+import vn.edu.leading.shop.services.MailService;
 import vn.edu.leading.shop.services.UserService;
 
 import javax.validation.Valid;
@@ -18,15 +19,19 @@ public class UserController {
 
     private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    private final MailService mailService;
+
+    public UserController(UserService userService, RoleRepository roleRepository, PasswordEncoder passwordEncoder, MailService mailService) {
         this.userService = userService;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
     }
 
     @PostMapping("/register")
     public String register(@Valid UserModel userModel) throws Exception {
         userService.register(userModel);
+        mailService.sendMail(userModel);
         return "redirect:/login";
     }
 }
